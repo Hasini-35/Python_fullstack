2a1753a8e985f8f183afaabbea32d8ba265e5383
Hasini-35 <hasinipolukonda25@gmail.com>
2026-08-03 08:44:02 +0000

    js

---
+++ js/script.js
@@ -0,0 +1,14 @@
+alert("Welcome to NRIIT Learing Management System")
+let heading = document.getElementById("welcome");
+heading.innerHTML = "Welcome Future Software Engineers"
+console.log("Heading element: ",heading)
+let msg=document.getElementById("message")
+msg.innerHTML="Javascript is fun"
+console.log("Message element:",msg)
+function showmessage() {
+    alert("Welcome to NRIIT Learning Management System")} 
+function changeHeading() {                                              
+    document.getElementById("welcome").innerHTML = "Welcome Python Fullstack Developers"}
+let button = docuemnt.getElementById("btnGreeting");
+button.addEventListener("click", function
+)
